d722f300dc825c72ae4c85be87a1cfe8106bf0af
iAngelManuel <iangelmanuel@hotmail.com>
2024-05-19 04:54:51 -0500

    chore: Add prettier-plugin-tailwindcss and @trivago/prettier-plugin-sort-imports to .prettierrc

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,17 +2,17 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
-import { fixupConfigRules } from '@eslint/compat'
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  pluginReactConfig,
 
   {
     rules: {
       'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
     },
   },
 ]
