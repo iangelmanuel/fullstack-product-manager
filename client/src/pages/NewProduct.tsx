def3d31e4e5a50fe19cda41173b223317e842cd0
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-20 00:10:47 -0500

    feat: Add ProductForm component to NewProductPage

--- client/src/pages/NewProduct.tsx
+++ client/src/pages/NewProduct.tsx
@@ -6,6 +6,7 @@ import {
   type ActionFunctionArgs,
 } from 'react-router-dom'
 import { ErrorMessage } from '../components/ErrorMessage'
+import { ProductForm } from '../components/ProductForm'
 import { addProduct } from '../services/product-service'
 
 export async function action({ request }: ActionFunctionArgs) {
@@ -44,30 +45,8 @@ export default function NewProductPage() {
       {error && <ErrorMessage>{error}</ErrorMessage>}
 
       <Form className="mt-10" method="POST">
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="name">
-            Nombre Producto:
-          </label>
-          <input
-            id="name"
-            type="text"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="price">
-            Precio:
-          </label>
-          <input
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-          />
-        </div>
+        <ProductForm />
+
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
