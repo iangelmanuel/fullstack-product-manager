019620956f0694f523fde4d4d0e3a1e68cbcfa5f
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-20 00:10:18 -0500

    feat: Add EditProduct and DeleteProduct routes and actions

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Layout from './Layouts/Layout'
+import { action as deleteProductAction } from './components/ProductDetails'
+import EditProduct, {
+  loader as editProductLoader,
+  action as editProductAction,
+} from './pages/EditProduct'
 import NewProductPage, {
   action as newProductAction,
 } from './pages/NewProduct'
-import ProductPage, { loader as productLoader } from './pages/Product'
+import ProductPage, {
+  loader as productLoader,
+  action as updateAvailabilityAction,
+} from './pages/Product'
 
 const router = createBrowserRouter([
   {
@@ -14,11 +22,22 @@ const router = createBrowserRouter([
         index: true,
         element: <ProductPage />,
         loader: productLoader,
+        action: updateAvailabilityAction,
       },
       {
-        path: 'product/create',
+        path: 'products/create',
         element: <NewProductPage />,
         action: newProductAction,
+      },
+      {
+        path: 'products/:id/edit',
+        element: <EditProduct />,
+        loader: editProductLoader,
+        action: editProductAction,
+      },
+      {
+        path: 'products/:id/delete',
+        action: deleteProductAction,
       },
     ],
   },
